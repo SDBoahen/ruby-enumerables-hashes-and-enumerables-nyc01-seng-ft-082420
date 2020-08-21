c71d76377387d672eeb5bc73099bbb9bd5c20db0
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-21 12:41:54 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,6 +4,17 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
+def 
+  
+  happy_birthday(birthday_kids)
+
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+   
+  winner
+
 end
